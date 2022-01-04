63f9b8196e48131995fd1f8f83202f2bbd9d3c4b
Jarek Šedý <jareksedy@icloud.com>
2022-01-04 17:36:02 +0600

    Added two scene folders & their view controllers.

---
+++ MVPExample/Scenes/GreetingScene/GreetingSceneViewController.swift
@@ -0,0 +1,15 @@
+//
+//  GreetingSceneViewController.swift
+//  MVPExample
+//
+//  Created by Ярослав on 04.01.2022.
+//
+
+import UIKit
+
+class GreetingSceneViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
---
+++ MVPExample/Scenes/InitialScene/InitialSceneViewController.swift
@@ -0,0 +1,15 @@
+//
+//  InitialSceneViewController.swift
+//  MVPExample
+//
+//  Created by Ярослав on 04.01.2022.
+//
+
+import UIKit
+
+class InitialSceneViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
